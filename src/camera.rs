8920e70a6a2617e31d861bb8c3ce4726fd6c0b73
Guillem Arias <guillem.arias@titanos.tv>
2024-02-16 15:58:23 +0100

    7. Moving Camera Code Into Its Own Class

---
+++ src/camera.rs
@@ -0,0 +1,102 @@
+use std::fs::File;
+use std::io::{self, Write};
+use crate::{color, sphere};
+use crate::color::Color;
+use crate::hittable::{HitRecord, Hittable};
+use crate::hittables::HittableList;
+use crate::interval::Interval;
+use crate::ray::Ray;
+use crate::vec3::{Point3, unit_vector, Vec3};
+
+pub(crate) struct Camera {
+    pub(crate) aspect_ratio: f64,
+    pub(crate) image_width: f64,
+    image_height: i32,
+    center: Point3,
+    pixel00_loc: Point3,
+    pixel_delta_u: Vec3,
+    pixel_delta_v: Vec3,
+}
+
+impl Camera {
+    pub fn new(aspect_ratio: f64, image_width: f64) -> Self {
+        Camera {
+            aspect_ratio,
+            image_width,
+            image_height: 0,
+            center: Point3::new(0.0, 0.0, 0.0),
+            pixel00_loc: Point3::new(0.0, 0.0, 0.0),
+            pixel_delta_u: Vec3::new(0.0, 0.0, 0.0),
+            pixel_delta_v: Vec3::new(0.0, 0.0, 0.0),
+        }
+    }
+
+    pub(crate) fn render(&mut self, world: &dyn Hittable) -> io::Result<()> {
+        Self::initialize(self);
+
+        let mut file = File::create("output.ppm")?;
+        writeln!(file, "P3\n{} {}\n255", self.image_width, self.image_height)?;
+
+        for h in 0..self.image_height {
+            eprintln!("Scanlines remaining: {} ", self.image_height - h);
+
+            for w in 0..self.image_width as i32 {
+                let pixel_center: Vec3 = self.pixel00_loc + w as f64 * self.pixel_delta_u + h as f64 * self.pixel_delta_v;
+                let ray_direction: Vec3 = pixel_center - self.center;
+                let ray: Ray = Ray::new(self.center, ray_direction);
+                let pixel_color: Color = Self::ray_color(&ray, world);
+
+                color::write_color(&mut file, pixel_color)?;
+            }
+        }
+        eprintln!("\nDone.");
+
+        Ok(())
+    }
+
+    fn initialize(&mut self) {
+        self.image_height = (self.image_width / self.aspect_ratio) as i32;
+
+        if self.image_height < 1 {
+            self.image_height = 1;
+        }
+
+        // Camera
+        const FOCAL_LENGTH: f64 = 1.0;
+        const VIEWPORT_HEIGHT: f64 = 2.0;
+        let viewport_width: f64 = VIEWPORT_HEIGHT * (self.image_width / self.image_height as f64);
+        self.center = Point3::new(0.0, 0.0, 0.0);
+
+        // Calculate the vectors across the horizontal and down the vertical viewport edges.
+        let viewport_u: Vec3 = Vec3::new(viewport_width, 0.0, 0.0);
+        let viewport_v: Vec3 = Vec3::new(0.0, -VIEWPORT_HEIGHT, 0.0);
+
+        // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+        self.pixel_delta_u = viewport_u / self.image_width as f64;
+        self.pixel_delta_v = viewport_v / self.image_height as f64;
+
+        // Calculate the location of the upper left pixel.
+        let viewport_upper_left: Vec3 = self.center - Vec3::new(0.0, 0.0, FOCAL_LENGTH) - viewport_u / 2.0 - viewport_v / 2.0;
+        self.pixel00_loc = viewport_upper_left + 0.5 * (self.pixel_delta_u + self.pixel_delta_v);
+    }
+
+    fn ray_color(ray: &Ray, world: &dyn Hittable) -> Color {
+        let mut hit_record: HitRecord = HitRecord::new(Point3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 0.0), 0.0, false);
+
+        if world.hit(ray, Interval::with_bounds(0.0, f64::INFINITY), &mut hit_record) {
+            let normal: Vec3 = hit_record.normal;
+            let white = Color::new(1.0, 1.0, 1.0);
+            return 0.5 * (normal + white);
+        }
+
+        let unit_direction: Vec3 = unit_vector(ray.direction());
+        const START_VALUE : f64 = 0.5;
+        const END_VALUE : f64 = 1.0;
+        let delta = START_VALUE * (unit_direction.y() + END_VALUE);
+
+        let white: Color = Color::new(1.0, 1.0, 1.0);
+        let blue: Color = Color::new(0.5, 0.7, 1.0);
+
+        return (END_VALUE - delta) * white + delta * blue;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,87 +6,29 @@ mod sphere;
 mod hittables;
 mod utils;
 mod interval;
+mod camera;
 
 use std::fs::File;
 use std::io::{self, Write};
 use color::{Color};
-use vec3::{Vec3, Point3, unit_vector};
+use vec3::{Vec3, Point3};
 use ray::{Ray};
-use hittable::{HitRecord, Hittable};
+use hittable::{Hittable};
 use hittables::{HittableList};
-use crate::interval::Interval;
+use crate::camera::Camera;
 
-fn ray_color(ray: &Ray, world: &dyn Hittable) -> Color {
-    let mut hit_record: HitRecord = HitRecord::new(Point3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 0.0), 0.0, false);
-
-    if world.hit(ray, Interval::with_bounds(0.0, f64::INFINITY), &mut hit_record) {
-        let normal: Vec3 = hit_record.normal;
-        let white = Color::new(1.0, 1.0, 1.0);
-        return 0.5 * (normal + white);
-    }
-
-    let unit_direction: Vec3 = unit_vector(ray.direction());
-    const START_VALUE : f64 = 0.5;
-    const END_VALUE : f64 = 1.0;
-    let delta = START_VALUE * (unit_direction.y() + END_VALUE);
-
-    let white: Color = Color::new(1.0, 1.0, 1.0);
-    let blue: Color = Color::new(0.5, 0.7, 1.0);
-
-    return (END_VALUE - delta) * white + delta * blue;
-}
-
-fn main() -> io::Result<()> {
-
-    // Image
-    const ASPECT_RATIO: f64 = 16.0 / 9.0;
-    const IMAGE_WIDTH: f64 = 400f64;
-
-    let mut image_height: i32 = (IMAGE_WIDTH / ASPECT_RATIO) as i32;
-
-    if image_height < 1 {
-        image_height = 1;
-    }
-
+fn main() {
     // World
     let mut world: HittableList = HittableList::new();
 
     world.add(Box::new(sphere::Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
     world.add(Box::new(sphere::Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
+    // Image
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: f64 = 400f64;
 
     // Camera
-    const FOCAL_LENGTH: f64 = 1.0;
-    const VIEWPORT_HEIGHT: f64 = 2.0;
-    let viewport_width: f64 = VIEWPORT_HEIGHT * (IMAGE_WIDTH / image_height as f64);
-    let camera_center: Point3 = Point3::new(0.0, 0.0, 0.0);
+    let mut camera = Camera::new(ASPECT_RATIO, IMAGE_WIDTH);
 
-    // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    let viewport_u: Vec3 = Vec3::new(viewport_width, 0.0, 0.0);
-    let viewport_v: Vec3 = Vec3::new(0.0, -VIEWPORT_HEIGHT, 0.0);
-
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    let pixel_delta_u: Vec3 = viewport_u / IMAGE_WIDTH;
-    let pixel_delta_v: Vec3 = viewport_v / image_height as f64;
-
-    // Calculate the location of the upper left pixel.
-    let viewport_upper_left: Vec3 = camera_center - Vec3::new(0.0, 0.0, FOCAL_LENGTH) - viewport_u / 2.0 - viewport_v / 2.0;
-    let pixel00_loc: Vec3 = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    let mut file = File::create("output.ppm")?;
-    writeln!(file, "P3\n{} {}\n255", IMAGE_WIDTH, image_height)?;
-
-    for h in 0..image_height {
-        eprintln!("Scanlines remaining: {} ", image_height - h);
-
-        for w in 0..IMAGE_WIDTH as i32 {
-            let pixel_center: Vec3 = pixel00_loc + w as f64 * pixel_delta_u + h as f64 * pixel_delta_v;
-            let ray_direction: Vec3 = pixel_center - camera_center;
-            let ray: Ray = Ray::new(camera_center, ray_direction);
-            let pixel_color: Color = ray_color(&ray, &world);
-
-            color::write_color(&mut file, pixel_color)?;
-        }
-    }
-    eprintln!("\nDone.");
-    Ok(())
+    let _ = camera.render(&world);
 }
